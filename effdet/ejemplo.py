8b871fe372dad31b9e5af92ae03908130da68f32
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-02-13 14:10:01 +0100

    código de ejemplo adaptado

--- effdet/ejemplo.py
+++ effdet/ejemplo.py
@@ -45,6 +45,35 @@ df_tr = pd.read_csv(dataset_path/"annotations/labelstrain.csv")
 df_ts = pd.read_csv(dataset_path/"annotations/labelstest.csv")
 df_vl = pd.read_csv(dataset_path/"annotations/labelsval.csv")
 
+def get_train_transforms(target_img_size=512):
+    # random angle, random blur, flip, random noise
+    # random combination of 3, random scale, random translation
+    return A.Compose(
+        [
+            A.HorizontalFlip(p=0.5),
+            A.Resize(height=target_img_size, width=target_img_size, p=1),
+            A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ToTensorV2(p=1),
+        ],
+        p=1.0,
+        bbox_params=A.BboxParams(
+            format="pascal_voc", min_area=0, min_visibility=0, label_fields=["labels"]
+        ),
+    )
+
+def get_valid_transforms(target_img_size=512):
+    return A.Compose(
+        [
+            A.Resize(height=target_img_size, width=target_img_size, p=1),
+            A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ToTensorV2(p=1),
+        ],
+        p=1.0,
+        bbox_params=A.BboxParams(
+            format="pascal_voc", min_area=0, min_visibility=0, label_fields=["labels"]
+        ),
+    )
+
 class EfficientDetDataset(Dataset):
     def __init__(self, dataset_adaptor, transforms=get_valid_transforms(), test=True):
         self.ds = dataset_adaptor
