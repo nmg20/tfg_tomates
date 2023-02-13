89b81bd1bdea0f5b84671e37fba2fb47e4d2bfb1
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-02-13 14:10:59 +0100

    versión para probar entrenamiento

--- effdet/ejemplo.py
+++ effdet/ejemplo.py
@@ -583,3 +583,49 @@ class EfficientDetModel(LightningModule):
                 scaled_bboxes.append(bboxes)
 
         return scaled_bbox
+
+dm = EfficientDetDataModule(train_dataset_adaptor=tomato_train_ds, 
+        validation_dataset_adaptor=tomato_train_ds,
+        num_workers=4,
+        batch_size=2)
+
+model = EfficientDetModel(
+    num_classes=1,
+    img_size=512
+    )
+
+from pytorch_lightning import Trainer 
+
+trainer = Trainer(
+        gpus=1, max_epochs=5, num_sanity_val_steps=1,
+    )
+trainer.fit(model,dm)
+
+# ######################
+
+img1, truth_bboxes1, _, _ = tomato_train_ds.get_image_and_labels_by_idx(0)
+img2, truth_bboxes2, _, _ = tomato_train_ds.get_image_and_labels_by_idx(1)
+
+images = [img1, img2]
+
+predicted_bboxes, predicted_class_confidences, predicted_class_labels = model.predict(images)
+
+def compare_bboxes_for_image(
+    image,
+    predicted_bboxes,
+    actual_bboxes,
+    draw_bboxes_fn=draw_pascal_voc_bboxes,
+    figsize=(20, 20),
+):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=figsize)
+    ax1.imshow(image)
+    ax1.set_title("Prediction")
+    ax2.imshow(image)
+    ax2.set_title("Actual")
+
+    draw_bboxes_fn(ax1, predicted_bboxes)
+    draw_bboxes_fn(ax2, actual_bboxes)
+
+    plt.show()
+
+compare_bboxes_for_image(img1, predicted_bboxes=predicted_bboxes[0],actual_bboxes=truth_bboxes1.tolist())
