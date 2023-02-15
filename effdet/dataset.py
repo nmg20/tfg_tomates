aa911ff21e184df1702c78e1f5f84992f8aae13d
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-02-15 16:45:09 +0100

    eliminada versión anterior de los módulos y reemplazada por la versión funcional más compacta

--- effdet/dataset.py
+++ effdet/dataset.py
@@ -1,16 +1,93 @@
-from pytorch_lightning import LightningDataModule
-from torch.utils.data import DataLoader
-# import EfficientDetDataset
+import matplotlib.pyplot as plt
+from matplotlib import patches
+
+def get_rectangle_edges_from_pascal_bbox(bbox):
+    xmin_top_left, ymin_top_left, xmax_bottom_right, ymax_bottom_right = bbox
+
+    bottom_left = (xmin_top_left, ymax_bottom_right)
+    width = xmax_bottom_right - xmin_top_left
+    height = ymin_top_left - ymax_bottom_right
+
+    return bottom_left, width, height
+
+def draw_pascal_voc_bboxes(
+    plot_ax,
+    bboxes,
+    get_rectangle_corners_fn=get_rectangle_edges_from_pascal_bbox,
+):
+    for bbox in bboxes:
+        bottom_left, width, height = get_rectangle_corners_fn(bbox)
+
+        rect_1 = patches.Rectangle(
+            bottom_left,
+            width,
+            height,
+            linewidth=4,
+            edgecolor="black",
+            fill=False,
+        )
+        rect_2 = patches.Rectangle(
+            bottom_left,
+            width,
+            height,
+            linewidth=2,
+            edgecolor="white",
+            fill=False,
+        )
+
+        # Add the patch to the Axes
+        plot_ax.add_patch(rect_1)
+        plot_ax.add_patch(rect_2)
+
+def show_image(
+    image, bboxes=None, draw_bboxes_fn=draw_pascal_voc_bboxes, figsize=(10, 10)
+):
+    fig, ax = plt.subplots(1, figsize=figsize)
+    ax.imshow(image)
+
+    if bboxes is not None:
+        draw_bboxes_fn(ax, bboxes)
+
+    plt.show()
+
+from pathlib import Path
+import PIL
+import numpy as np
+
+class TomatoDatasetAdaptor:
+    def __init__(self, images_dir_path, annotations_dataframe):
+        self.images_dir_path = Path(images_dir_path)
+        self.annotations_df = annotations_dataframe
+        self.images = self.annotations_df.image.unique().tolist()
+
+    def __len__(self) -> int:
+        return len(self.images)
+
+    def get_image_and_labels_by_idx(self, index):
+        image_name = self.images[index]
+        image = PIL.Image.open(self.images_dir_path / image_name)
+        pascal_bboxes = self.annotations_df[self.annotations_df.image == image_name][
+            ["xmin", "ymin", "xmax", "ymax"]
+        ].values
+        class_labels = np.ones(len(pascal_bboxes))
+
+        return image, pascal_bboxes, class_labels, index
+    
+    def show_image(self, index):
+        image, bboxes, class_labels, image_id = self.get_image_and_labels_by_idx(index)
+        print(f"image_id: {image_id}")
+        show_image(image, bboxes.tolist())
+        print(class_labels)
+
+# tomato_train_ds = TomatoDatasetAdaptor(train_data_path, df)
+# cars_train_ds.show_image(0)
+# cars_train_ds.show_image(3)
 
 from torch.utils.data import Dataset
-
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 
-import torch utils.
 def get_train_transforms(target_img_size=512):
-    # random angle, random blur, flip, random noise
-    # random combination of 3, random scale, random translation
     return A.Compose(
         [
             A.HorizontalFlip(p=0.5),
@@ -23,6 +100,7 @@ def get_train_transforms(target_img_size=512):
             format="pascal_voc", min_area=0, min_visibility=0, label_fields=["labels"]
         ),
     )
+
 
 def get_valid_transforms(target_img_size=512):
     return A.Compose(
@@ -81,6 +159,9 @@ class EfficientDetDataset(Dataset):
 
     def __len__(self):
         return len(self.ds)
+
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader
 
 class EfficientDetDataModule(LightningDataModule):
     
