1828a3af8bd607061584277a81e7ff634c9f8bac
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-02-22 19:34:45 +0100

    simplificada la función de entrenamiento y la de generación de un modelo(pasar a Model.py)

--- effdet/Train.py
+++ effdet/Train.py
@@ -4,8 +4,10 @@ import pandas as pd
 from EffDetDataset import *
 from Model import *
 import torch
-from torch.utils.tensorboard import SummaryWriter
 import torchvision
+from pytorch_lightning import Trainer 
+
+from torch.utils.tensorboard import SummaryWriter
 
 writer = SummaryWriter('lightning_logs/tomato_exp1')
 
@@ -29,21 +31,10 @@ dm = EfficientDetDataModule(train_dataset_adaptor=train_ds,
         num_workers=4,
         batch_size=2)
 
-model_architecture="tf_efficientnetv2_l"
-wbf_iou_threshold=0.25
-prediction_confidence_threshold=0.15
-
-model = EfficientDetModel(
-    model_architecture=model_architecture,
-    wbf_iou_threshold=wbf_iou_threshold,
-    prediction_confidence_threshold=prediction_confidence_threshold
-    )
-
 ############################
 
 def get_model(
-        arch="tf_efficientnetv2_l",
-        iou=0.3,
+        iou=0.44,
         confd=0.2):
     return EfficientDetModel(model_architecture=arch,
         wbf_iou_threshold=iou,
@@ -54,12 +45,13 @@ def train_model(model=model, dm=dm,num_epochs=5):
         gus=1, num_epochs=num_epochs,num_sanity_val_steps=1)
     trainer.fit(model,dm)
 
+model = get_model()
 
 ########################################
 # Entrenamiento
 ########################################
 
-from pytorch_lightning import Trainer 
+
 
 # trainer = Trainer(
 #         gpus=1, max_epochs=5, num_sanity_val_steps=1,
@@ -67,17 +59,18 @@ from pytorch_lightning import Trainer
 # trainer.fit(model,dm)
 # torch.save(model.state_dict(),f"{model_architecture}_{wbf_iou_threshold}iou_{prediction_confidence_threshold}confidence.pth")
 
-model.load_state_dict(torch.load("modelos/ED_20ep_0.3iou_0.2cf.pt"))
+# model.load_state_dict(torch.load("modelos/ED_20ep_0.3iou_0.2cf.pt"))
+model.load_state_dict(torch.load("modelos/ED_20ep_0.25iou_0.2cf.pt"))
 
 # model.eval()
 
-loader = dm.val_dataloader()
-dl_iter = iter(loader)
+# loader = dm.val_dataloader()
+# dl_iter = iter(loader)
 # batch = next(dl_iter)
-images, targets, _, _ = next(dl_iter)
-device = model.device;device
+# images, targets, _, _ = next(dl_iter)
+# device = model.device;device
 # output = model.validation_step(batch=batch,batch_idx=0)
 # output
 
-img_grid = torchvision.utils.make_grid(images)
-writer.add_image('tomato_images', img_grid)
+# img_grid = torchvision.utils.make_grid(images)
+# writer.add_image('tomato_images', img_grid)
