e169c5063978bbd9ac943828df955d7494b18c8d
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-03-02 13:21:07 +0100

    dataset que utilizar como parámetro -> nombre para guardarlo tb

--- effdet/CL_Train.py
+++ effdet/CL_Train.py
@@ -7,6 +7,9 @@ import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
 import argparse
+import os
+
+models_dir = "modelos/"
 
 def validate_file(f):
     if not os.path.exists(f):
@@ -21,27 +24,50 @@ def dir_path(path):
     else:
         raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
 
-models_dir = "modelos/"
+def load_dss(path):
+    dataset_path = Path(path)
+    train_data_path = dataset_path/"images/train/"
+    test_data_path = dataset_path/"images/test/"
+    val_data_path = dataset_path/"images/val/"
+    df_tr = pd.read_csv(dataset_path/"annotations/labelstrain.csv")
+    df_ts = pd.read_csv(dataset_path/"annotations/labelstest.csv")
+    df_vl = pd.read_csv(dataset_path/"annotations/labelsval.csv")
 
-dataset_path = Path("../../tomates512/")
-train_data_path = dataset_path/"images/train/"
-test_data_path = dataset_path/"images/test/"
-val_data_path = dataset_path/"images/val/"
+    train_ds = TomatoDatasetAdaptor(train_data_path, df_tr)
+    test_ds = TomatoDatasetAdaptor(test_data_path, df_ts)
+    val_ds = TomatoDatasetAdaptor(val_data_path, df_vl)
+    return train_ds, test_ds, val_ds
 
-df_tr = pd.read_csv(dataset_path/"annotations/labelstrain.csv")
-df_ts = pd.read_csv(dataset_path/"annotations/labelstest.csv")
-df_vl = pd.read_csv(dataset_path/"annotations/labelsval.csv")
+# def get_dm(train,val):
+    # return EfficientDetDataModule(train_dataset_adaptor=train, 
+    #     validation_dataset_adaptor=val,
+    #     num_workers=4,
+    #     batch_size=2)
 
-train_ds = TomatoDatasetAdaptor(train_data_path, df_tr)
-test_ds = TomatoDatasetAdaptor(test_data_path, df_ts)
-val_ds = TomatoDatasetAdaptor(val_data_path, df_vl)
+def get_dm(path):
+    dataset_path = Path(path)
+    train_data_path = dataset_path/"images/train/"
+    test_data_path = dataset_path/"images/test/"
+    val_data_path = dataset_path/"images/val/"
+    df_tr = pd.read_csv(dataset_path/"annotations/labelstrain.csv")
+    df_ts = pd.read_csv(dataset_path/"annotations/labelstest.csv")
+    df_vl = pd.read_csv(dataset_path/"annotations/labelsval.csv")
+
+    train_ds = TomatoDatasetAdaptor(train_data_path, df_tr)
+    test_ds = TomatoDatasetAdaptor(test_data_path, df_ts)
+    val_ds = TomatoDatasetAdaptor(val_data_path, df_vl)
+    return EfficientDetDataModule(train_dataset_adaptor=train_ds, 
+        validation_dataset_adaptor=val_ds,
+        test_dataset_adaptor=test_ds,
+        num_workers=4,
+        batch_size=2)
 
 ############################
 
-dm = EfficientDetDataModule(train_dataset_adaptor=train_ds, 
-        validation_dataset_adaptor=val_ds,
-        num_workers=4,
-        batch_size=2)
+# dm = EfficientDetDataModule(train_dataset_adaptor=train_ds, 
+#         validation_dataset_adaptor=val_ds,
+#         num_workers=4,
+#         batch_size=2)
 
 # torch.save(model.state_dict(),model_name)
 
@@ -53,32 +79,39 @@ def main():
     parser = argparse.ArgumentParser()
     # parser.add_argument('-a', '--architecture', help="Tipo de arquitectura del modelo",
         # type=str)
-    parser.add_argument('-d', '--d', type=int, help="Número de épocas de entrenamiento.")
+    # parser.add_argument('-d', '--d', type=int, help="Número de épocas de entrenamiento.")
+    parser.add_argument('-d','--dataset',type=dir_path, help="Directorio del dataset a partir del que crear el dataframe.")
     parser.add_argument('-e', '--epochs', type=int, help="Número de épocas de entrenamiento.")
     # parser.add_argument('-t','--thresh',type=float, help="Umbral de wbf_iou.")
     # parser.add_argument('-sk','--skip',type=float, help="Nivel de iou mínimo en la predicción.")
     # parser.add_argument('-cd','--predconfd', type=float, help="idk man.")
     # parser.add_argument('-s','--save', type=int, help="Nombre con el que guardar el modelo entrenado.")
-    parser.add_argument('-n','--name', type=str, help="Nombre con el que guardar el modelo entrenado.")
+    # parser.add_argument('-n','--name', type=str, help="Nombre con el que guardar el modelo entrenado.")
+    parser.add_argument('-n','--name',type=str)
     args = parser.parse_args()
-    if args.d :
-        model = EfficientDetModel(
-        model_architecture="tf_efficientnetv2_b0")
-    else:
-        model = EfficientDetModel(
-            # model_architecture=f"tf_efficientnetv2_{args.architecture}",
-            # wbf_iou_threshold=args.thresh,
-            # skip_box_thr=args.skip,
-            # prediction_confidence_threshold=args.predconfd
-            )
-    logger = TensorBoardLogger("lightning_logs/",name=f"ED_{args.epochs}ep_{args.name}")
+    # if args.d :
+    #     model = EfficientDetModel(
+    #     model_architecture="tf_efficientnetv2_b0")
+    # else:
+    dm = get_dm(args.dataset)
+    model = EfficientDetModel(
+        # model_architecture=f"tf_efficientnetv2_{args.architecture}",
+        # wbf_iou_threshold=args.thresh,
+        # skip_box_thr=args.skip,
+        # prediction_confidence_threshold=args.predconfd
+        )
+    # logger = TensorBoardLogger("lightning_logs/",name=f"ED_{args.epochs}ep_{args.name}")
+    
+    model_name = f"ED_{args.epochs}ep"
+    if args.name!="no":
+        model_name = args.name+model_name
+    logger = TensorBoardLogger("lightning_logs/",name=model_name)
     trainer = Trainer(
         gpus = 1, max_epochs=args.epochs, num_sanity_val_steps=1, logger=logger,
     )
     trainer.fit(model,dm)
-    # if args.save:
-    model_name = f"{models_dir}ED_{args.epochs}ep_{args.name}.pt"
-    torch.save(model.state_dict(),model_name)
+
+    torch.save(model.state_dict(),f"{models_dir}{model_name}.pt")
 
 
 
