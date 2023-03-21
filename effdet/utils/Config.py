a4b46a684b3afc9ce055de06d702c3c2ab9bcb2d
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-03-21 12:32:05 +0100

    versión en pruebas de módulo para leer ficheros de configuración

--- effdet/utils/Config.py
+++ effdet/utils/Config.py
@@ -3,3 +3,22 @@ file = "model.config"
 config = configparser.ConfigParser()
 config.read(file)
 config.sections()
+
+"""
+Una config para cada dataset (?).
+Also para cada modelo.
+"""
+
+def get_params(section):
+	if section not in config.sections():
+		print(f"No section {section} on {file}.\n")
+	else:
+		d = {}
+		for opt in cf.options(section):
+			r[opt]=cf.get(section,opt)
+		return r
+
+
+def __init__():
+	config = configparser.ConfigParser()
+	config.read_file(file)
