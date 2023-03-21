09cfdf3c144cc9ef36d243833b7db50fc0f1b501
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-03-21 20:32:01 +0100

    limpieza de código

--- effdet/Test.py
+++ effdet/Test.py
@@ -3,8 +3,11 @@ from pytorch_lightning import Trainer
 
 from torch.utils.tensorboard import SummaryWriter
 
-def test_model(model, dm,num_epochs=1):
+def test_model(model, dm):
     trainer = Trainer(
         gpus=1, max_epochs=num_epochs,num_sanity_val_steps=1)
         # gpus=1, num_epochs,num_sanity_val_steps=1)
     trainer.test(model,dataloaders=dm.test_dataloader())
+
+def main():
+    parser = argparse.ArgumentParser()
