9e01a48f963c903b88e889661dad7ae35c559c38
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-03-22 13:14:17 +0100

    simplificada inicialización de Trainer

--- effdet/Test.py
+++ effdet/Test.py
@@ -5,7 +5,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 def test_model(model, dm):
     trainer = Trainer(
-        gpus=1, max_epochs=num_epochs,num_sanity_val_steps=1)
+        gpus=1)
         # gpus=1, num_epochs,num_sanity_val_steps=1)
     trainer.test(model,dataloaders=dm.test_dataloader())
 
