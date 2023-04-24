57faa70b3dfe6e820d28c0b8f7f5cc63f74d1880
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-04-24 21:27:58 +0200

    versión con registro de histogramas de áreas de bboxes y confianzas de cada predicción

--- effdet/Inference.py
+++ effdet/Inference.py
@@ -2,9 +2,10 @@ from pathlib import Path
 import pandas as pd
 from EffDetDataset import *
 from Model import *
-from utils.Visualize import uniquify_dir
+from utils.Visualize import uniquify_dir, draw_image
 import os
 import argparse
+from torch.nn import CrossEntropyLoss as CE
 
 preds_dir = "./preds/"
 data_dir = os.path.abspath("./data/")
@@ -56,7 +57,25 @@ def get_dir_imgs_names(imgs_dir=data_dir):
 #     anots = [read_anots(x) for x in images]
 #     save_preds(images,anots,preds_dir+output_dir)
 
-def inference_v2(model,data_dir=data_dir,output_dir=output_dir):
+# def get_hist(data):
+#     bins = len(list(set(data)))
+#     counts, bins, _ = plt.hist(data,bins=bins)
+
+# def save_confs_and_areas(bboxes, confs):
+#     areas, cfs = [], []
+#     for bbox in bboxes:
+#         w,h = get_bbox_dim(bbox)
+#         areas.append(w*h)
+
+def save_hist(data, name):
+    fig, ax = plt.subplots(figsize=(20,20))
+    plt.hist(data)
+    plt.savefig(name)
+
+def bbox_area(bbox):
+    return (bbox[2]-bbox[0])*(bbox[3]-bbox[1])
+
+def inference(model,data_dir=data_dir,output_dir=output_dir):
     """
     A partir de un directorio crea un ds ad-hoc, lee de un fichero maestro
     las anotaciones, infiere los resultados y os vuelca en un directorio "output_dir".
@@ -65,7 +84,17 @@ def inference_v2(model,data_dir=data_dir,output_dir=output_dir):
     """
     # Output_dir con un dir /runx (x=número único -> uniquify)
     output=Path(uniquify_dir(output_dir+"/run"))
+    os.mkdir(output)
     data_ds = get_data_ds(get_dir_imgs_names())
+    model.eval()
+    images, anots = [i for i,_,_,_ in data_ds.get_imgs_and_anots()],[i for _,i,_,_ in data_ds.get_imgs_and_anots()]
+    bboxes,_,confs = model.predict(images)
+    criterion = CE()
+    for image, bbox, conf in zip(images, bboxes, confs):
+        name = str(output)+"/"+image.filename.split("/")[-1].split(".")[0]
+        save_hist([bbox_area(x) for x in bbox],name+"_areas_hist.png")
+        save_hist(conf, name+"_confs_hist.png")
+        draw_image(image, bbox, conf, name)
 
 
 """
@@ -73,7 +102,8 @@ data_ds = get_data_ds(get_dir_imgs_names())
 model=get_model()
 load_ex_model(model,"d801010")
 model.eval()
-imgs,anots = map(list,zip(*data_ds.get_imgs_and_anots()))
+#imgs,anots = map(list,zip(*data_ds.get_imgs_and_anots()))
+imgs, anots = [i for i,_,_,_ in data_ds.get_imgs_and_anots()],[i for _,i,_,_ in data_ds.get_imgs_and_anots()]
 bboxes,_,confs = model.predict(imgs)
 
 
