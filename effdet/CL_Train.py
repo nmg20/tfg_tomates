14d3465294ec8da873252cf643d0423bf4c41a4c
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-05-20 21:49:19 +0200

    debug de áreas de las bounding boxes

--- effdet/CL_Train.py
+++ effdet/CL_Train.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from EffDetDataset import *
 from Model import *
 from Train import *
+from Inference import file_to_bboxes, save_hist
 import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
@@ -25,16 +26,29 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-e', '--epochs', type=int, help="Número de épocas de entrenamiento.")
     parser.add_argument('-n','--name',type=str)
+    parser.add_argument('-d','--debug',type=int, help="Flag para debuggear registrando el área de los bounding boxes.")
     args = parser.parse_args()
     dm = get_dm_standalone(dataset_dir,args.name)
-    model = EfficientDetModel()
+    #DEBUG
+    if args.debug :
+        data_file = open("outputs/data.dat","w")
+    else:
+        data_file = False
+    model = EfficientDetModel(data_file)
     model_name = args.name
     # model_name = f"{args.epochs}eps"
     # if args.name!="no":
     #     model_name = f"{args.name}_{model_name}"
     logger = TensorBoardLogger("lightning_logs/",name=model_name)
     train_model(model,dm,args.epochs,logger)
-    torch.save(model.state_dict(),f"{models_dir}{model_name}.pt")
+    if args.debug==0 :
+        torch.save(model.state_dict(),f"{models_dir}{model_name}.pt")
+    else:
+        data_file.close()
+        save_hist(file_to_bboxes("outputs/data.dat"),f"outputs/Áreas Training {name}.png",100)
+        
+
+        
 
 
 if __name__=="__main__":
