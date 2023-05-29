17f6271606fb471af826edad8361b7452430dd10
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-05-29 18:44:17 +0200

    comentada sección de debug de las bounding boxes

--- effdet/CL_Train.py
+++ effdet/CL_Train.py
@@ -10,7 +10,7 @@ import argparse
 import os
 
 models_dir = "modelos/"
-dataset_dir = "../../datasets/Tomato_1280x720/"
+main_ds = "../../datasets/Tomato_1280x720/"
 
 def validate_file(f):
     if not os.path.exists(f):
@@ -26,26 +26,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-e', '--epochs', type=int, help="Número de épocas de entrenamiento.")
     parser.add_argument('-n','--name',type=str)
-    parser.add_argument('-d','--debug',type=int, help="Flag para debuggear registrando el área de los bounding boxes.")
+    parser.add_argument('-s','--save',type=str, help="Flag para guardar el modelo entrenado")
+    # parser.add_argument('-d','--debug',type=str, help="Flag para debuggear registrando el área de los bounding boxes.")
     args = parser.parse_args()
-    dm = get_dm_standalone(dataset_dir,args.name)
+    dm = get_dm_standalone(main_ds,args.name)
     #DEBUG
-    if args.debug :
-        data_file = open("outputs/data.dat","w")
-    else:
-        data_file = False
-    model = EfficientDetModel(data_file)
+    # if args.debug :
+    #     data_file = open(args.debug,"w")
+    # else:
+    #     data_file = False
+    data_file = False
+    # model = EfficientDetModel(data_file)
+    model = EfficientDetModel()
     model_name = args.name
-    # model_name = f"{args.epochs}eps"
-    # if args.name!="no":
-    #     model_name = f"{args.name}_{model_name}"
     logger = TensorBoardLogger("lightning_logs/",name=model_name)
     train_model(model,dm,args.epochs,logger)
-    if args.debug==0 :
+    if args.save==1:
         torch.save(model.state_dict(),f"{models_dir}{model_name}.pt")
-    else:
-        data_file.close()
-        save_hist(file_to_bboxes("outputs/data.dat"),f"outputs/Áreas Training {name}.png",100)
+    # if args.debug:
+        # data_file.close()
+        # save_hist(file_to_bboxes(args.debug),f"outputs/Áreas Training {name}.png",100)
         
 
         
