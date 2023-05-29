5d6e748b736e1c0f61fede060ce280717d62138f
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-05-29 18:44:58 +0200

    añadido flag de 'profiler' al Trainer

--- effdet/Train.py
+++ effdet/Train.py
@@ -2,6 +2,6 @@ from Model import *
 from pytorch_lightning import Trainer 
 
 def train_model(model, dm,num_epochs=1,logger=None):
-    trainer = Trainer(gpus=1, max_epochs=num_epochs,num_sanity_val_steps=1,logger=logger,)
+    trainer = Trainer(gpus=1, max_epochs=num_epochs,num_sanity_val_steps=1,logger=logger,profiler="simple")
         # gpus=1, num_epochs,num_sanity_val_steps=1)
     trainer.fit(model,dm)
