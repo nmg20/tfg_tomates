747e447e8d74226f85914f23541fc10c6933d462
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-07-13 13:39:23 +0200

    cambiado número de épocas (revisar)

--- effdet/Train.py
+++ effdet/Train.py
@@ -1,7 +1,8 @@
 from Model import *
 from pytorch_lightning import Trainer 
 
-def train_model(model, dm,num_epochs=1,logger=None):
-    trainer = Trainer(gpus=1, max_epochs=num_epochs,num_sanity_val_steps=1,logger=logger,profiler="simple")
+def train_model(model, dm, num_epochs,logger=None):
+    # trainer = Trainer(gpus=1, max_epochs=num_epochs,num_sanity_val_steps=1,logger=logger,profiler="simple")
+    trainer = Trainer(gpus=1, max_epochs=num_epochs,num_sanity_val_steps=1,logger=logger)
         # gpus=1, num_epochs,num_sanity_val_steps=1)
     trainer.fit(model,dm)
