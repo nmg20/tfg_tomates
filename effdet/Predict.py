d46a6ee40051201eec4653386b0cf35c5c05197b
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-07-26 13:14:18 +0200

    archivo a modo de fichero de configuración con las rutas a los directorios usados por cada módulo

--- effdet/Predict.py
+++
@@ -1,7 +0,0 @@
-from Model import *
-from pytorch_lightning import Trainer 
-
-def predict_model(model, dm,num_epochs=1,logger=None):
-    trainer = Trainer(gpus=1, max_epochs=num_epochs,logger=logger,profiler="simple")
-        # gpus=1, num_epochs,num_sanity_val_steps=1)
-    trainer.predict(model,dataloaders=dm.pred_dataloaders())
