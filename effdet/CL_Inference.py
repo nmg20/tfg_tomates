9c085df202d77959b1a492e100d14512052c7b7f
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-07-26 13:08:41 +0200

    eliminado flag para guardar gráfica de loss del proceso de inferencia

--- effdet/CL_Inference.py
+++ effdet/CL_Inference.py
@@ -29,15 +29,14 @@ def main():
     
         -> añadir un flag para simplemente registrar la loss.
     """
+    #$python CL_Inference.py -m d801010 -f test.txt
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--model', type=str, help="Número de épocas de entrenamiento.")
-    # parser.add_argument('-c', '--compare', default=False, action='store_true')
     parser.add_argument('-f', '--file', type=str, help="Fichero del que leer los nombres de las imágenes.")
-    parser.add_argument('-l', '--loss', type=int, help="Flag para que la inferenci sólo registre la loss para cada imagen.")
+    # parser.add_argument('-l', '--loss', type=int, help="Flag para que la inferenci sólo registre la loss para cada imagen.")
 
     args = parser.parse_args()
-    # inference(args.model, args.compare)
-    inference(args.model, args.file, args.loss)
+    inference(args.model, args.file)
 
 if __name__=="__main__":
     main()
