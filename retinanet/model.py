e7bb4dc0f26087af3b3c0f875a8ac7c58aea767b
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-09-18 18:25:27 +0200

    versión de prueba del modelo retinanet

--- retinanet/model.py
+++ retinanet/model.py
@@ -17,25 +17,25 @@ class RetinaNetModel(LightningModule):
         num_classes=1,
         image_mean=None,
         image_std=None,
-        score_thresh=0.05,
+        score_thresh=0.05,inference_transforms=get_pred_transforms(target_img_size=512),
     ):
         super().__init__()
         self.model = retinanet_resnet50_fpn(weights=RetinaNet_ResNet50_FPN_Weights.DEFAULT)
 
-        def forward(self, images):
-            return self.model(images)
+    def forward(self, images):
+        return self.model(images)
 
-        def training_step(self, batch, batch_idx):
-            images, bboxes, labels = batch
-            outputs = self.model(images)
-            loss = F.cross_entropy(outputs, bboxes)
-            self.training_step_outputs.append(outputs)
-            self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-            return loss
+    def training_step(self, batch, batch_idx):
+        images, bboxes, labels = batch
+        outputs = self.model(images)
+        loss = F.cross_entropy(outputs, bboxes)
+        self.training_step_outputs.append(outputs)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
 
-        @torch.no_grad()
-        def validation_step(self, batch, batch_idx):
-            images, bboxes, labels = batch
-            outputs = self.model(images)
-            loss = F.cross_entropy(outputs, bboxes)
-            self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+    @torch.no_grad()
+    def validation_step(self, batch, batch_idx):
+        images, bboxes, labels = batch
+        outputs = self.model(images)
+        loss = F.cross_entropy(outputs, bboxes)
+        self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
