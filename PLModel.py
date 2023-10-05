010c566126e81b91d143ecbf6ba9827d896d8ec6
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-10-05 22:27:33 +0200

    actualizada función de forward del modelo abstracto (cambiar a un LightningModule para cada modelo)

--- PLModel.py
+++ PLModel.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
-from pytorch_lightning import LightningModule
-import lightning.pytorch as pl
+from lightning.pytorch import LightningModule
 
 from torchvision.models import resnet50, ResNet50_Weights
 from torchvision.models import mobilenet_v2, MobileNet_V2_Weights
@@ -27,7 +26,7 @@ class AbstractPLModel(nn.Module):
             self.model = ssd300_vgg16(weights=SSD300_VGG16_Weights.DEFAULT)
         elif backbone == "resnet":
             self.model = resnet50(weights=ResNet50_Weights.DEFAULT)
-            num_features = self.backbone.fc.in_features
+            num_features = self.model.fc.in_features
             self.model.fc = nn.Linear(num_features, num_classes)
         elif backbone == "retinanet":
             self.model = retinanet_resnet50_fpn(weights=RetinaNet_ResNet50_FPN_Weights.DEFAULT)
@@ -36,10 +35,13 @@ class AbstractPLModel(nn.Module):
 
         self.threshold = torch.nn.Sigmoid()
 
-    def forward(self, x):
-        return self.model.forward(x)
+    def forward(self, images, targets=None):
+        outputs = self.model(images, targets)
+        if not self.training or targets is None:
+            outputs[0]['scores'] = self.threshold(outputs[0]['scores'])
+        return outputs
 
-class TomatoLightning(pl.LightningModule):
+class TomatoLightning(LightningModule):
     """
     Clase que contiene el funcionamiento básico de un modelo a efectos de entrenamiento/validación/test.
     Crea un modelo en concreto en base al backbone específicado.
