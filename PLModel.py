b195e6c5289ef5fdcb9b047c5910b25920bf035e
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-10-05 14:12:22 +0200

    cambio en el import de pytorch_lightning y función de congelado de módulos para un modelo

--- PLModel.py
+++ PLModel.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from pytorch_lightning import LightningModule
+import lightning.pytorch as pl
 
 from torchvision.models import resnet50, ResNet50_Weights
 from torchvision.models import mobilenet_v2, MobileNet_V2_Weights
@@ -10,31 +11,7 @@ from torchvision.models.detection import retinanet_resnet50_fpn, RetinaNet_ResNe
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
 from torchvision.models.detection import fcos_resnet50_fpn, FCOS_ResNet50_FPN_Weights
 
-# class FasterRCNNThres(FasterRCNN):
-#     """
-#     Modificación de FasterRCNN con una variable entrenable a modo de 
-#     umbral de resultados.
-#     """
-#     def __init__(self, num_classes):
-#         backbone = fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
-#         backbone.out_channels = 2048
-#         super().__init__(backbone, num_classes, 
-#             rpn_anchor_generator = AnchorGenerator(
-#                 sizes=((32, 64, 128, 256, 512),), 
-#                 aspect_ratios=((0.5, 1.0, 2.0),) * 5)
-#             ,
-#             box_roi_pooler = torchvision.ops.MultiScaleRoIAlign(
-#                 featmap_names=['0'],
-#                 output_size=7,
-#                 sampling_ratio=2)
-#             )
-#         self.threshold = torch.nn.Sigmoid()
-
-#     def forward(self, images, targets=None):
-#         outputs = super().forward(images, targets)
-#         if not self.training or targets is None:
-#             outputs[0]['scores'] = self.threshold(outputs[0]['scores'])
-#         return outputs
+from lightning.pytorch import Trainer
 
 class AbstractPLModel(nn.Module):
     """
@@ -44,25 +21,25 @@ class AbstractPLModel(nn.Module):
     def __init__(self, backbone, num_classes=1):
         super(AbstractPLModel, self).__init__()
         if backbone=="fasterrcnn":
-            self.backbone = fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
-            self.backbone.out_channels = 2048
+            self.model = fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
+            self.model.out_channels = 2048
         elif backbone == "ssd":
-            self.backbone = ssd300_vgg16(weights=SSD300_VGG16_Weights.DEFAULT)
+            self.model = ssd300_vgg16(weights=SSD300_VGG16_Weights.DEFAULT)
         elif backbone == "resnet":
-            self.backbone = resnet50(weights=ResNet50_Weights.DEFAULT)
+            self.model = resnet50(weights=ResNet50_Weights.DEFAULT)
             num_features = self.backbone.fc.in_features
-            self.backbone.fc = nn.Linear(num_features, num_classes)
+            self.model.fc = nn.Linear(num_features, num_classes)
         elif backbone == "retinanet":
-            self.backbone = retinanet_resnet50_fpn(weights=RetinaNet_ResNet50_FPN_Weights.DEFAULT)
+            self.model = retinanet_resnet50_fpn(weights=RetinaNet_ResNet50_FPN_Weights.DEFAULT)
         elif backbone=="fcos":
-            self.backbone = fcos_resnet50_fpn(weights=FCOS_ResNet50_FPN_Weights.DEFAULT)
+            self.model = fcos_resnet50_fpn(weights=FCOS_ResNet50_FPN_Weights.DEFAULT)
 
         self.threshold = torch.nn.Sigmoid()
 
     def forward(self, x):
         return self.model.forward(x)
 
-class TomatoLightning(LightningModule):
+class TomatoLightning(pl.LightningModule):
     """
     Clase que contiene el funcionamiento básico de un modelo a efectos de entrenamiento/validación/test.
     Crea un modelo en concreto en base al backbone específicado.
@@ -77,7 +54,7 @@ class TomatoLightning(LightningModule):
         super().__init__()
         self.num_classes = num_classes
         self.lr = lr
-        self.model = AbstractPLModel(num_classes)
+        self.model = AbstractPLModel(backbone, num_classes)
 
     def forward(self, images, targets):
         return self.model(images, targets)
@@ -108,3 +85,11 @@ class TomatoLightning(LightningModule):
         loss = F.cross_entropy(targets['bboxes'], bboxes)
         self.log('test_loss', loss)
         return loss 
+
+def freeze_modules(model, modules=["regression_head"]):
+    for name, module in model.named_modules():
+        if any(module_name in name for module_name in modules):
+            for param in module.parameters():
+                param.requires_grad = False
+            if len(list(module.children()))>0:
+                freeze_modules(module, modules)
