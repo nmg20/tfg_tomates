1d6162f6cb74708ff55668117b89e045c7bdc386
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-11-14 15:00:18 +0100

    arreglos para prevenir que dos tensores estén en 'devices' distintos + adaptación para el fichero de configuración

--- modelos/utils.py
+++ modelos/utils.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import CrossEntropyLoss as CE
+from torch.nn import CrossEntropyLoss
 from ensemble_boxes import ensemble_boxes_wbf
 import os
 
@@ -10,30 +10,9 @@ from torchvision.ops import box_iou, complete_box_iou_loss as iou_loss
 from torchvision.ops import sigmoid_focal_loss, boxes as box_ops, complete_box_iou_loss as iou_loss
 from torchmetrics import Precision, Recall, Accuracy
 
-# os.system("wget https://raw.githubusercontent.com/pytorch/vision/main/references/detection/engine.py")
-# os.system("wget https://raw.githubusercontent.com/pytorch/vision/main/references/detection/utils.py")
-# os.system("wget https://raw.githubusercontent.com/pytorch/vision/main/references/detection/coco_utils.py")
-# os.system("wget https://raw.githubusercontent.com/pytorch/vision/main/references/detection/coco_eval.py")
-# os.system("wget https://raw.githubusercontent.com/pytorch/vision/main/references/detection/transforms.py")
-
-# def compute_loss(predictions, targets):
-#     """
-#     box_iou([N,4],[M,4]) -> NxM
-#     indexes = box_iou(b1,b2).max(dim=1)
-#     """
-#     losses = []
-#     total = 0.
-#     for prediction, target in zip(predictions, targets):
-#         p_box, p_scores, p_labels = prediction['boxes'], prediction['scores'], prediction['labels']
-#         t_box, t_labels = target['boxes'], target['labels']
-#         iou = box_iou(t_box, p_box)
-#         best_iou, indexes = iou.max(dim=1)
-#         # class_loss = sigmoid_focal_loss(p_box[indexes],t_box,reduction="sum")
-#         class_loss = sigmoid_focal_loss(p_box[indexes],t_box,reduction="sum")
-#         box_loss = F.l1_loss(p_box[indexes],t_box, reduction="sum")
-#         total += class_loss + box_loss
-#         losses.append((class_loss, box_loss))
-#     return losses, total
+import sys
+sys.path.append("..")
+import config
 
 def resize_boxes(boxes, sizes):
     new_boxes = []
@@ -68,10 +47,11 @@ def image_sizes(images):
         sizes.append((w,h))
     return sizes
 
-def threshold_fusion(outputs, sizes, iou_thr, skip_box_thr):
+def threshold_fusion(outputs, images, iou_thr, skip_box_thr):
     #Dados los resultados del modelo, los divide en bboxes, scores y labels,
     #aplica wbf con umbralización, los convierte otra vez a tensores y los devuelve
     detections = []
+    sizes = image_sizes(images)
     for output, size in zip(outputs, sizes):
         #Paso a arrays
         boxes = output['boxes'].detach().cpu().numpy()
@@ -84,9 +64,9 @@ def threshold_fusion(outputs, sizes, iou_thr, skip_box_thr):
             [labels.tolist()],
             iou_thr=iou_thr, skip_box_thr=skip_box_thr)
         detections.append({
-            'boxes': torch.tensor(upsize_boxes(boxes, size)),
-            'scores': torch.tensor(np.array(scores)),
-            'labels': torch.tensor(np.array([int(x) for x in labels]))
+            'boxes': torch.tensor(upsize_boxes(boxes, size)).to(config.DEVICE),
+            'scores': torch.tensor(np.array(scores)).to(config.DEVICE),
+            'labels': torch.tensor(np.array([int(x) for x in labels])).to(config.DEVICE)
         })
     return detections
 
@@ -96,11 +76,12 @@ def compute_single_loss(boxes1, boxes2, labels1, labels2):
     Usamos CrossEntropyLoss para el error de clase y
     Complete_Box_IoU_Loss para el error de regresión de las bboxes.
     """
+    ce = CrossEntropyLoss(reduction="mean")
     _, indices = box_iou(boxes2, boxes1).max(dim=1)
-    # class_loss = CE(labels1.float(), labels2[indices].float())
-    box_loss = iou_loss(boxes1, boxes2)
-    # return class_loss, box_loss
-    return box_loss
+    class_loss = ce(labels1[indices].float(), labels2.float())
+    box_loss = iou_loss(boxes1[indices], boxes2, reduction="mean")
+    return class_loss, box_loss
+    # return box_loss
 
 def compute_loss(detections, targets):
     """
@@ -114,27 +95,32 @@ def compute_loss(detections, targets):
         - 'labels'
     """
     total_loss = 0
+    class_losses, box_losses = 0, 0
     for detection, target in zip(detections, targets):
         p_boxes, t_boxes = detection['boxes'], target['boxes']
+        p_boxes, t_boxes = p_boxes.detach().cpu(), t_boxes.detach().cpu()
         p_labels, t_labels = detection['labels'], target['labels']
+        p_labels, t_labels = p_labels.detach().cpu(), t_labels.detach().cpu()
         class_loss, box_loss = compute_single_loss(
             p_boxes, t_boxes,
             p_labels, t_labels
         )
         total_loss += total_loss + class_loss + box_loss
-    return total_loss
+        class_losses += class_losses + class_loss
+        box_losses += box_losses + box_loss
+    return {'total': total_loss, 'class': class_losses, 'box': box_losses}
 
-def get_metrics(detection, target):
-    """
-    Devuelve el cálculo de las métricas de Precisión, Recall y Accuracy
-    para una detección.
-    """
-    _, indices = box_iou(target, detection).max(dim=1)
-    metrics = {
-        'precision': Precision(detection, target),
-        'recall': Recall(detection, target),
-        'accuracy': Accuracy(detection, target)
-    }
+# def get_metrics(detection, target):
+#     """
+#     Devuelve el cálculo de las métricas de Precisión, Recall y Accuracy
+#     para una detección.
+#     """
+#     _, indices = box_iou(target, detection).max(dim=1)
+#     metrics = {
+#         'precision': Precision(detection, target),
+#         'recall': Recall(detection, target),
+#         'accuracy': Accuracy(detection, target)
+#     }
 
 # def compute_metrics(detections, targets):
 #     """
