ba746c86e1b58c378dfb83f3071838ef57fc05f6
Nicolás Martínez González <n.gonzalezm@udc.es>
2023-11-14 15:05:26 +0100

    fichero de configuración general del sistema

---
+++ config.py
@@ -0,0 +1,33 @@
+import torch
+
+#Parámetros de entrenamiento
+NUM_CLASSES = 1
+LR = 0.0002
+NUM_EPOCHS = 30
+IOU_THR = 0.5
+
+#Parámetros del DataModule
+DS_DIR = "../datasets/T1280x720/"
+IMAGES_DIR = DS_DIR + "JPEGImages/"
+IMAGESETS_DIR = DS_DIR + "ImageSets/"
+BATCH_SIZE = 4
+NUM_WORKERS = 8
+
+#Cuda/CPU
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+if torch.cuda.is_available():
+    torch.set_float32_matmul_precision('medium') 
+
+#Outputs
+OUTPUTS_DIR = "./outputs"
+
+#Modelos
+MODELS_DIR = "./pths/"
+
+#Logs
+LOGS_DIR = "./logs/"
+
+#MeanAP
+KEYS = ['map','map_50','map_75','map_small','map_medium','map_large',
+    'mar_1','mar_10','mar_100','mar_small','mar_medium','mar_large',
+    'map_per_class', 'mar_100_per_class']
